929933634ecc2ce0a62a73809a81153400905088
sunilmandle <49607870+sunilmandle@users.noreply.github.com>
2019-04-20 19:38:35 +0530

    Updated omdbapi file

--- omdbapi.sh
+++ omdbapi.sh
@@ -2,7 +2,12 @@
 echo "Enter the movie name: "
 read Movie
 URL="http://www.omdbapi.com/?apikey=bc2a4edf&t=$Movie"
-fetchdata=`curl -s $URL` 
+if [ -z $URL ]
+then 
+  echo "exit"
+else
+  fetchdata=`curl -s $URL` 
+fi
 Name=`echo $fetchdata | jq .Title`
 Ratings=`echo $fetchdata | jq .Ratings[1].Value`
 echo "Name of the movie = $Name"
